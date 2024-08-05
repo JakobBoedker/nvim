e78221a452241adb963efc87fb0f18741a21e4bf
jakobboedker <bdkerjakob1@gmail.com>
2024-08-05 11:30:29 +0200

    First commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,17 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+vim.opt.swapfile = false
+vim.cmd("set clipboard=unnamedplus")
+vim.keymap.set('n', '<leader>h', ':set nohlsearch<CR>')
+-- navigate between windows
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+
